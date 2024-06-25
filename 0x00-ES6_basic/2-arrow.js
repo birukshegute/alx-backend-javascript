7a6a5f63dc9084d3ff098d2aa1a18dbff83f2e16
birukshegute <birukshegute@gmail.com>
2024-06-25 11:02:27 +0300

    Idk what is going on

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -2,7 +2,6 @@ export default function getNeighborhoodsList() {
     this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
   
     const self = this;
-    /*eslint-disable */
     this.addNeighborhood = (newNeighborhood) => {
       self.sanFranciscoNeighborhoods.push(newNeighborhood);
       return self.sanFranciscoNeighborhoods;
