0488aa4bbfd04252c74b83d7bc286edb0fd7f757
birukshegute <birukshegute@gmail.com>
2024-06-27 23:48:17 -0700

    eslint fixes

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,19 +1,19 @@
 export default class Building {
-    constructor(sqft) {
-      if (
-        this.constructor !== Building
+  constructor(sqft) {
+    if (
+      this.constructor !== Building
         && typeof this.evacuationWarningMessage !== 'function'
-      ) {
-        throw Error(
-          'Class extending Building must override evacuationWarningMessage',
-        );
-      }
-  
-      this._sqft = sqft;
+    ) {
+      throw Error(
+        'Class extending Building must override evacuationWarningMessage',
+      );
     }
-  
-    // sqft
-    get sqft() {
-      return this._sqft;
-    }
+
+    this._sqft = sqft;
   }
+
+  // sqft
+  get sqft() {
+    return this._sqft;
+  }
+}
