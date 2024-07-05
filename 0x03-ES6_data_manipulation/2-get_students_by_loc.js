16d7daa15bbddcfaac36ff4b893b9d7523a194dc
birukshegute <birukshegute@gmail.com>
2024-07-05 11:26:08 +0300

    testing task 2

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,7 +1,5 @@
 export default function getStudentsByLocation(array, city){
-  if (array instanceof Array && city instanceof String) {
-    array.filter((items) => items.firstName);
-    array.filter((city) => city.location );
-    return array; 
+  if (city instanceof String) {
+    return array.filter((city) => array.location === city );
   }
 }
