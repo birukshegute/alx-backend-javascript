63a8901e6c6c7dee04f6f2ff32d8055fa9388693
birukshegute <birukshegute@gmail.com>
2024-07-05 10:36:43 +0300

    testing task 1

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,7 +1,3 @@
-export default function getListStudentIds(){
-  const students = 
-    [{ id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-    { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' }]
-  return students.map(items => items.id)
+export default function getListStudentIds(Array){
+  return Array.map(items => items.id)
 }
